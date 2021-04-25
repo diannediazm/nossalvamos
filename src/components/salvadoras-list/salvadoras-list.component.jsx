3fbd742cbf5c092c03495c9f13832ef0bde433b0
Dianne <dianne.diazm@gmail.com>
2021-04-25 15:42:20 -0400

    responsive

--- src/components/salvadoras-list/salvadoras-list.component.jsx
+++ src/components/salvadoras-list/salvadoras-list.component.jsx
@@ -4,15 +4,17 @@ import SalvadorasItem from "../salvadoras-item/salvadoras-item.component";
 import "./salvadoras-list.styles.scss";
 
 const SalvadorasList = ({ title, items }) => (
-  <Grid container>
-    <div className="lista">
-      <h1 className="lista-text">{title.toUpperCase()}</h1>
-      <div>
-        {items.map(({ id, ...otherItemProps }) => (
-          <SalvadorasItem key={id} {...otherItemProps} />
-        ))}
+  <Grid container spacing={3} justify="center">
+    <Grid item xs={12} sm={6} md={4}>
+      <div className="lista">
+        <h1 className="lista-text">{title.toUpperCase()}</h1>
+        <div>
+          {items.map(({ id, ...otherItemProps }) => (
+            <SalvadorasItem key={id} {...otherItemProps} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Grid>
   </Grid>
 );
 
